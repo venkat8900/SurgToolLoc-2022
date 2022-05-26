0887c4c35370be122d0b819278c1bde99866a07e
venkat8900 <venkatmukthineni@gmail.com>
2022-05-26 13:52:04 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SurgToolLoc 
+
+## Generate frames from video clips
+
+```
+python extract_frames.py
+```
+This creates a new folder named data in which subfolders with video clip names are present. Each subfolder contain 9 images from corresponding video clip.  
---
+++ extract_frames.py
@@ -0,0 +1,54 @@
+import cv2
+import os
+import glob
+
+def extract_frames_from_single_video(video_path, save_folder):
+    """
+    : video_path: location of the video
+    : save_folder: folder to save extracted frames
+    Creates a seperate folder for frames. 
+    Picks one frame every 10 frames. Creates 180 images
+    """
+
+    video = cv2.VideoCapture(video_path)
+
+    current_frame = 0
+
+    while(True):
+        ret, frame = video.read()
+
+        if not os.path.exists(save_folder + '/' + str(video_path[-15:-4]) + '/'):
+            print("Creating Save directory for frame: ", video_path[-15:-4])
+            os.makedirs(save_folder + '/' + str(video_path[-15:-4]) + '/')
+
+        if ret:
+            current_frame += 1
+            if (current_frame % 300) == 0:
+
+                frame_name = save_folder + '/' + str(video_path[-15:-4])+ '/' + str(video_path[-15:-4]) + '_' + str(current_frame//300) + '.jpg'
+                print("Creating frame: " + frame_name)
+
+                cv2.imwrite(frame_name, frame)
+        
+        else:
+            break
+
+
+def extract_frames():
+    """
+    Extract frames from surgtoolloc2022_dataset folder and saves 180 frames form \
+    every video in data folder
+    """
+
+    video_list = []
+
+    video_folder = './surgtoolloc2022_dataset/_release/training_data/video_clips/*.mp4'
+    video_save_path = './data'
+
+    for file in glob.iglob(video_folder, recursive=True):
+        video_list.append(file)
+
+    for video_path in video_list:
+        extract_frames_from_single_video(video_path, video_save_path)
+
+extract_frames()
